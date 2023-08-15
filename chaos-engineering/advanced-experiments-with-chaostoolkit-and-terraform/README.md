88ba126e076bf744d876ab72583036c388d3f695
Manuel Castellin <manuel@castellinconsulting.com>
2023-08-15 06:40:45 +0200

    feat: add article files for chaostoolkit-terraform

---
+++ chaos-engineering/advanced-experiments-with-chaostoolkit-and-terraform/README.md
@@ -0,0 +1,4 @@
+# Advanced Chaos Experiments with ChaosToolkit and Terraform
+
+**Related Articles**
+* [Orchestrating Advanced Experiments with ChaosToolkit and Terraform](https://devlearnops.com/advanced-experiments-with-chaostoolkit-and-terraform)
---
+++ chaos-engineering/advanced-experiments-with-chaostoolkit-and-terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = var.container_name
+
+  restart = var.restart_policy
+
+  ports {
+    internal = 80
+    external = var.exposed_port
+  }
+}
