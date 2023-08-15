483ce7be427f000f6b42ada86ac6560a7b7dafb2
Manuel Castellin <manuel@castellinconsulting.com>
2023-08-15 09:13:52 +0200

    add windows instructions

--- chaos-engineering/advanced-experiments-with-chaostoolkit-and-terraform/main.tf
+++ chaos-engineering/advanced-experiments-with-chaostoolkit-and-terraform/main.tf
@@ -11,6 +11,18 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
+##################################################################
+# For Windows users:
+# 
+# Make sure you enable the "Expose daemon on tcp://localhost:2375
+# without TLS" option in Docker settings and replace the docker
+# provider with the below
+##################################################################
+#
+#provider "docker" {
+#  host = "tcp://127.0.0.1:2375"
+#}
+
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
